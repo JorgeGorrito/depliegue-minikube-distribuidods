87b17b748fcad3741f8f455f7c372411d38e77a2
JorgeGorrito <j0rg3.4b3ll4@gmail.com>
2023-02-18 03:57:40 -0500

    Se corrigio bug de servicio

--- busroute.sh
+++ busroute.sh
@@ -1,9 +1,20 @@
-kubectl delete -f back-deployment.yaml 
-kubectl delete -f front-deployment.yaml 
-kubectl delete -f service.yaml 
+kubectl delete -f back-deployment.yaml;
+kubectl delete -f front-deployment.yaml; 
+kubectl delete -f back-service.yaml;
+kubectl delete -f front-service.yaml; 
+
+kubectl delete pod --all;
+kubectl get pods
+kubectl get services
+
 
 kubectl apply -f back-deployment.yaml
 kubectl apply -f front-deployment.yaml
 
-kubectl apply -f service.yaml
-minikube service front-back-service --url
+kubectl apply -f back-service.yaml
+kubectl apply -f front-service.yaml
+
+echo 'url-front:'
+minikube service front-service --url
+echo 'url-back:'
+minikube service back-service --url
